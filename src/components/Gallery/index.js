b10ed93f5e206050b333839993c416fb67b3182c
daejo <daejo@icloud.com>
2020-12-08 22:35:32 -0800

    adds photolist component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,50 +1,16 @@
-import React, { useState } from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
-const [categories] = useState([
-  {
-    name: 'commercial',
-    description: 'Photos of grocery stores, food trucks, and other commercial projects',
-  },
-  { name: 'portraits', description: 'Portraits of people in my life' },
-  { name: 'food', description: 'Delicious delicacies' },
-  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
-
+function Gallery({ currentCategory }) {
+  const { name, description } = currentCategory;
   return (
-    <header className="flex-row px-1">
-      <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a href="#about">
-              About me
-            </a>
-          </li>
-          <li>
-            <span>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
-              <span onClick={() => { setCurrentCategory(category) }}>
-                {capitalizeFirstLetter(category.name)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList  />
+    </section>
   );
 }
 
-export default Nav;
+export default Gallery;

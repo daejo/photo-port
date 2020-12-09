c19c15257dca3947131854a5ac79d0389c878a97
daejo <daejo@icloud.com>
2020-12-09 00:55:05 -0800

    sets up contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,6 +18,7 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" rows="5"  />
                 </div>
+                <button type="submit">Submit</button>
             </form>
         </section>
       )

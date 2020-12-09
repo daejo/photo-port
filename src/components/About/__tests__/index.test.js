7f8304dc97ee7362f2074824391cae28d2d7aad3
daejo <daejo@icloud.com>
2020-12-08 19:22:48 -0800

    fixes tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -5,18 +5,14 @@ import About from '..';
 
 afterEach(cleanup);
 
-//* RENDERS ABOUT TEST *//
 describe('About component', () => {
-    // First Test
-    it('renders', () => {
-        render(<About />);
-    });
-
-    // Second Test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    })
-});
-
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
